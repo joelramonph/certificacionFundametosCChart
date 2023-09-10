5cba1f98abae81d7440b012a2e03dc6d5e3484d1
Joel Parra <joelramonph@gmail.com>
2023-09-10 07:03:51 -0400

    archivo matriz

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿ // Egercicio 1 Creacion y asunacion de matrix
+ // string[] fraudulentOrderIDs = new string[3];
+
+ // fraudulentOrderIDs[0] = "A123";
+ // fraudulentOrderIDs[1] = "B456";
+ // fraudulentOrderIDs[2] = "C789";
+ // fraudulentOrderIDs[3] = "D000";
+
+// string[] fraudulentOrderIDs = {"A123", "B456", "C789"};
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+ // Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+ // fraudulentOrderIDs[0] = "F000";
+
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+
+ // Console.WriteLine($"Hay {fraudulentOrderIDs.Length} ordenes fraudulemtas en proceso");
+
+ // Ejercicio 2 recorrido con instruccion foreach
+
+ // Productos terminados en cada contnedor
+
+// int[] inventario = {200,450, 700,125,250};
+
+// int suma = 0;
+// int bin = 0;
+// foreach (var items in inventario)
+// {
+//     bin++;
+//     suma += items;
+// Console.WriteLine($"Bin {bin} = {items} items (Running total: {suma})");
+// }
+// Console.WriteLine($"Tenemos {suma} productos en inventario");
+
+// Crear una variable que contenga el número del contenedor actual y que muestre la suma acumulativa
+
